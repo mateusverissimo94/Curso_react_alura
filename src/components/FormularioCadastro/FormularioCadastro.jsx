75bee5e564083c7969e1a034312808b08954e9de
Mateus Barros <mateusverissimo94@gmail.com>
2021-10-27 13:00:12 -0300

    Primeiro curso de ReactJS - Alura

---
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -0,0 +1,50 @@
+import React, { Component } from "react";
+import "./estilo.css";
+class FormularioCadastro extends Component {
+
+  constructor(props) {
+    super(props);
+    this.titulo = "";
+    this.texto = "";
+  }
+
+  _handleMundancaTitulo(evento) {
+    evento.stopPropagation();
+    this.titulo = evento.target.value;
+  }
+
+  _handleMundancaTexto(evento) {
+    evento.stopPropagation();
+    this.texto = evento.target.value;
+  }
+
+  _criarNota(evento) {
+    evento.preventDefault();
+    evento.stopPropagation();
+    this.props.criarNota(this.titulo, this.texto);
+  }
+
+  render() {
+    return (
+      <form className="form-cadastro" onSubmit={this._criarNota.bind(this)}>
+        <input
+          type="text"
+          placeholder="Título"
+          className="form-cadastro_input"
+          onChange={this._handleMundancaTitulo.bind(this)}
+        />
+        <textarea
+          rows={15}
+          placeholder="Escreva sua nota..."
+          className="form-cadastro_input"
+          onChange={this._handleMundancaTexto.bind(this)}
+        />
+        <button className="form-cadastro_input form-cadastro_submit">
+          Criar Nota
+        </button>
+      </form>
+    );
+  }
+}
+
+export default FormularioCadastro;
